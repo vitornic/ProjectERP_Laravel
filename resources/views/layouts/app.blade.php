fe38812485f2478032f9ca3f6608df14030881f3
João Vitor <clauzer75@hotmail.com>
2020-07-03 19:23:48 -0300

    att

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,6 +47,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-pjax-version" content="{{ mix('css/app.css') }}">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
