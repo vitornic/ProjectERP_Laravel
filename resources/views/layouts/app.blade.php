440014452a59715feeb4973da1377dcc5ab7d9d1
João Vitor <clauzer75@hotmail.com>
2020-07-05 17:16:26 -0300

    fix layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,7 +53,7 @@
 .fix {
     float: left;
     position: relative;
-    width: 83.7vh;
+    width: 83.7%;
     padding: 10px;
 }
 
@@ -62,7 +62,7 @@
     position: relative;
     width: auto;
     padding: 14px;
-    max-width: 83.7vh;
+    max-width: 83.7%;
 }
 
 </style>
